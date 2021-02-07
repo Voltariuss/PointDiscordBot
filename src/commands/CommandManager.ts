b1a4210526e4d009661b488bf1b29cc1157feb5f
Voltariuss <loic.duboistermoz@gmail.com>
2021-02-07 18:26:39 +0100

    adds message content spliter for command args recovering

--- src/commands/CommandManager.ts
+++ src/commands/CommandManager.ts
@@ -3,6 +3,10 @@ class CommandManager {
     public static isCommand(msgContent: string): boolean {
         return msgContent.startsWith('!')
     };
+
+    public static getCommandArgs(cmdContent: string) {
+        return cmdContent.split(' ');
+    }
 }
 
 export { CommandManager };
