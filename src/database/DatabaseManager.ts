8bd90710c2a15fe024b0d5e86cef9dc2913cb2f1
Voltariuss <loic.duboistermoz@gmail.com>
2021-02-14 19:29:45 +0100

    adds transaction management with SQLite

--- src/database/DatabaseManager.ts
+++ src/database/DatabaseManager.ts
@@ -3,16 +3,43 @@ import * as _ from 'lodash';
 
 class DatabaseManager {
 
-  public static isTableExists(database: Database, tableName: string, callback: (isExists: boolean) => void): void {
-    database.get('SELECT name FROM sqlite_master WHERE type=$type AND name=$name', {
-      $type: 'table',
-      $name: tableName
-    }, (err: Error, row: any) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      const isExists: boolean = !_.isEmpty(row);
-      callback(isExists);
+  public static ERROR_START_TRANSACTION: string = 'Failed to start the transaction.';
+  public static ERROR_COMMIT_TRANSACTION: string = 'Failed to commit the transaction.';
+  public static ERROR_ROLLBACK_TRANSACTION: string = 'Failed to rollback the transaction.';
+
+  public static startTransaction(database: Database): Promise<void> {
+    return new Promise((resolve: () => void, reject: (reason?: any) => void) => {
+      database.run('BEGIN', (err: Error) => {
+        if (err) {
+          console.error(this.ERROR_START_TRANSACTION);
+          reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  public static commitTransaction(database: Database): Promise<void> {
+    return new Promise((resolve: () => void, reject: (reason?: any) => void) => {
+      database.run('COMMIT', (err: Error) => {
+        if (err) {
+          console.error(this.ERROR_COMMIT_TRANSACTION);
+          reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  public static rollbackTransaction(database: Database): Promise<void> {
+    return new Promise((resolve: () => void, reject: (reason?: any) => void) => {
+      database.run('ROLLBACK', (err: Error) => {
+        if (err) {
+          console.error(this.ERROR_ROLLBACK_TRANSACTION);
+          reject(err);
+        }
+        resolve();
+      });
     });
   }
 }
